84ab25fb5fecd39d96790db8052daf574aafd1c8
pietro <pietro.g.moroni@gmail.com>
2024-05-20 10:59:21 +0200

    hp :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl">Innovation Project (OTB)</h1>
+      <h2 className="text-xl">Leadership & Innovation 2024</h2>
+      <h3 className="text-lg">Group 25</h3>
+      <Link href="/landing" className="text-sky-500 hover:text-orange-500">
+        Go to DPP landing page
+      </Link>
+      <Link href="/bravery" className="text-sky-500 hover:text-orange-500">
+        Go to customer app
+      </Link>
+    </main>
+  );
 }
